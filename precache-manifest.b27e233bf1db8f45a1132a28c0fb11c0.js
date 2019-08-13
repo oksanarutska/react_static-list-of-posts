3ccd999bf7c4f3a95664b737a8c4d6576ece482f
oksanarutska <42785757+oksanarutska@users.noreply.github.com>
2019-08-13 17:36:10 +0300

    Updates

---
+++ precache-manifest.b27e233bf1db8f45a1132a28c0fb11c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1519850663556a7b83c6b1af3735a245",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "bf820ef8f03cf84e53df",
+    "url": "/react_static-list-of-posts/static/css/main.cf4515a8.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/react_static-list-of-posts/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "bf820ef8f03cf84e53df",
+    "url": "/react_static-list-of-posts/static/js/main.0d9a6fd7.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
